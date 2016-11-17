f6e142e6626adbdebc5b60ff114992dce43b7aa3
lenovo1 <lenovo1@lenovo.lan>
2016-11-17 23:40:43 +0200

    importer db connection

--- src/test/java/services/Importer.java
+++ src/test/java/services/Importer.java
@@ -1,5 +1,11 @@
 package services;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import junit.framework.TestCase;
 
 import org.apache.tapestry5.hibernate.modules.HibernateCoreModule;
@@ -18,13 +24,54 @@ public class Importer extends TestCase{
 	private static final Logger logger = LoggerFactory.getLogger(Importer.class);
 
 	
-	public void testImportOld(){
+	public void testImportOld() throws SQLException{
 		logger.info("starting");
 		RegistryBuilder builder = new RegistryBuilder();		 
 		builder.add(WebsiteModule.class,TapestryModule.class,HibernateCoreModule.class);		 
-		Registry registry = builder.build();	 
+		Registry registry = builder.build();	
+		
 		registry.performRegistryStartup();
+		
+		String nodeCount = getNodesCount();
+		processNodesandCategories();
+		logger.info("nodes {}",nodeCount);
 		logger.info("done");
+	}
+
+
+	private void processNodesandCategories() throws SQLException {
+		Connection connection = getConnection();
+		
+	}
+
+
+	private Connection getConnection() throws  SQLException {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return  DriverManager.getConnection("jdbc:mariadb://localhost:3306/atmoments", "root", "");  
+		
+	}
+
+
+	private String getNodesCount() {
+		
+	        try {
+				Connection connection = this.getConnection(); 				
+				PreparedStatement st = connection.prepareStatement("select count(*) from node");
+				ResultSet res = st.executeQuery();
+				if(res.next()){
+		           return res.getString(1);
+		        }
+				
+	        
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}  
+	      
+	        return "failed";
 	}
 	
 }
