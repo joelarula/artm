a2c74f0ce557c445f7ba9be7b5124320b530d84a
lenovo1 <lenovo1@lenovo.lan>
2016-12-12 01:06:31 +0200

    run as single jar

--- src/main/java/website/services/WebsiteModule.java
+++ src/main/java/website/services/WebsiteModule.java
@@ -15,9 +15,12 @@ import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.ScopeConstants;
 import org.apache.tapestry5.ioc.ServiceBinder;
+import org.apache.tapestry5.ioc.annotations.ImportModule;
 import org.apache.tapestry5.ioc.annotations.Startup;
+import org.apache.tapestry5.ioc.annotations.SubModule;
 import org.apache.tapestry5.ioc.services.RegistryShutdownHub;
 import org.apache.tapestry5.services.assets.AssetRequestHandler;
+import org.apache.tapestry5.upload.modules.UploadModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,6 +34,7 @@ import website.services.impl.FileManagerImpl;
 import website.services.impl.ImageDispatcher;
 import website.services.impl.ModelDaoImpl;
 
+@ImportModule(UploadModule.class)
 public class WebsiteModule {
 
 	private static final Logger logger = LoggerFactory.getLogger(WebsiteModule.class);
